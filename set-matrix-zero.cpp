e03eefcf9924ea3c37679f0c7d80a9ef3e33bccf
Mann-gupta1 <137791573+Mann-gupta1@users.noreply.github.com>
2023-07-03 22:40:22 +0530

    Update set-matrix-zero.cpp

--- set-matrix-zero.cpp
+++ set-matrix-zero.cpp
@@ -1,1 +1,35 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeros(vector<vector<int>> &matrix)
+{
+    int R = matrix.size();
+    int C = matrix[0].size();
+    vector<bool> rowZeros(R, false);
+    vector<bool> colZeros(C, false);
+
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                rowZeros[i] = true;
+                colZeros[j] = true;
+            }
+        }
+    }
+
+    
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            if (rowZeros[i] || colZeros[j])
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
