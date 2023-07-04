0d89d52afce864807e2ec783a3c36cd639c70219
Mann-gupta1 <137791573+Mann-gupta1@users.noreply.github.com>
2023-07-04 17:44:34 +0530

    Create max-area.cpp

---
+++ max-area.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int max_area = 0;
+                int i = 0, j = n - 1;
+
+        while (i < j) {
+            int area = min(height[i], height[j]) * (j - i);
+            max_area = max(max_area, area);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max_area;
+    }
+};
